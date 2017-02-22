0dd330992ca6b95551945f1cafd08066d89a7f81
cnlohr <lohr85@gmail.com>
2017-02-21 21:10:30 -0500

    update my sdkconfig for a single-layer board mode.

--- main/app_main.c
+++ main/app_main.c
@@ -28,12 +28,50 @@
 
 #include "i2s_stream_out.h"
 
+
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+
 void asmtest();
 
 uint32_t mydata;
 
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    return ESP_OK;
+}
+
 void app_main()
 {
+#define DO_WIFI
+#ifdef DO_WIFI
+
+	nvs_flash_init();
+	tcpip_adapter_init();
+	ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+	wifi_config_t sta_config = {
+		.sta = {
+		    .ssid = "charles",
+		    .password = "wifimadeeasy",
+		    .bssid_set = false
+		}
+	};
+	ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
+	ESP_ERROR_CHECK( esp_wifi_start() );
+	ESP_ERROR_CHECK( esp_wifi_connect() );
+#endif
+
+
+
 
 #define I2SOTEST
 
